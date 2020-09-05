21b1c16020a222c882d7c99e516312646487c222
Ngoan Tran <ngoan98travinh@gmail.com>
2020-09-05 08:22:03 +0700

    refactor: rename vars

--- src/index.js
+++ src/index.js
@@ -4,17 +4,16 @@ const consoleLog = require('./middleware/console-log');
 
 const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const nextApp = next({ dev });
+const nextHandle = nextApp.getRequestHandler();
 
-app.prepare().then(() => {
-  const server = express();
+nextApp.prepare().then(() => {
+  const expressApp = express();
 
-  server.use(consoleLog);
+  expressApp.use(consoleLog);
 
-  server.get('*', (req, res) => handle(req, res));
+  expressApp.get('*', (req, res) => nextHandle(req, res));
 
-  server.listen(port);
-
+  expressApp.listen(port);
   console.log(`> Ready on http://localhost:${port}`);
 });
